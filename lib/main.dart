b029de46d0885982482590e303581c2ba8aabba4
kgoel085 <kgoel085@gmail.com>
2021-06-26 22:40:12 +0530

    Added question component

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return MyAppState();
-    // throw UnimplementedError();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion () {
+  void _answerQuestion () {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
     print('Answer choosen');
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -33,10 +32,12 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(children: 
           <Widget>[
-            Text(questions[questionIndex]),
+            Question(
+              questions[_questionIndex]
+            ),
             ElevatedButton(
               child: Text('Answer 1'), 
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             ElevatedButton(
               child: Text('Answer 2'),
